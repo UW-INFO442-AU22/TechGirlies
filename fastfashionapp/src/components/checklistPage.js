1519aec556f0f4ebeba1d78abd21b4beca85def8
Roshni Srikanth <rsrika@uw.edu>
2022-11-01 14:49:37 -0700

    checklist

--- fastfashionapp/src/components/checklistPage.js
+++ fastfashionapp/src/components/checklistPage.js
@@ -8,52 +8,20 @@ export default function Checklist() {
             
             <h1>Are Your Clothes Sustainable?</h1>
            
-            <div className="container">
-                <div className="row">
-                    <div className="col">
-                        <img className="checklist-diagram" src="./img/logo.png" alt="logo"/>
-                        <h4>RESEARCH BRAND</h4>
-                        <p>Is the company is green-certified? See who their suppliers and maufacturers are, and see what the media is saying about it. </p>
-                        <br></br>
-                    </div>
-
-                    <div className="col">
-                        <img className="checklist-diagram" src="./img/logo.png" alt="logo"/>
-                        <h4>FABRIC SHEERNESS</h4>
-                        <p>If you can hold a garment up to the light and can't see the outline of your hand through the material, it suggests that the garment will be more durable over time.</p>
-                        <br></br>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <img className="checklist-diagram" src="./img/logo.png" alt="logo"/>
-                        <h4>SEAMS & STITCHES</h4>
-                        <p>Tighter, more frequent stitches usually make for a stronger seam. More durable clothing should usually have hardier stitches with no discernible gaps.</p>
-                        <br></br>
-                    </div>
-                    <div className="col">
-                        <img className="checklist-diagram" src="./img/logo.png" alt="logo"/>
-                        <h4>TAG & MATERIALS</h4>
-                        <p>Check the tag to see if the garment is made out of sustainable materials, if it’s locally made, and how to care for it to make it last. </p>
-                        <br></br>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <img className="checklist-diagram" src="./img/logo.png" alt="logo"/>
-                        <h4>DENIM STIFFNESS</h4>
-                        <p>Check to see how stiff a pair of jeans are. High quality denim is usually heavier and a bit stiff at first because of the material's higher thread count.</p>
-                        <br></br>
-                    </div>
-
-                    <div className="col">
-                        <img className="checklist-diagram" src="./img/logo.png" alt="logo"/>
-                        <h4>STRETCH TEST</h4>
-                        <p>High-quality clothing material usually has good "recovery," which means that the fibers spring back into shape and maintain their form after being stretched.</p>
-                        <br></br>
-                    </div>
-                </div>
+            <div className="container checklist-start">
+                
             </div>
         </div>
     )
+}
+
+export function ChecklistItems(item) {
+    return (
+        <div className="col">
+            <img className="checklist-diagram" alt="" src={item.img_source}/>
+            <h4>{item.title}</h4>
+            <p>{item.description}</p>
+            <br></br>
+        </div>
+    );
 }
