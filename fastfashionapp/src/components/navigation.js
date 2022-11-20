cab8ce2b46f156f1571f5293322a49e353e8bbfd
Madi Wake <madiwake@icloud.com>
2022-11-20 14:16:13 -0800

    more documentation - all documentation except for findStyles page and genrePage done

--- fastfashionapp/src/components/navigation.js
+++ fastfashionapp/src/components/navigation.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useState } from "react";
 import { Link } from 'react-router-dom';
 
+// manually creating a hamburger menu icon
 function HamburgerMenu(){
     return(
         <div>
@@ -12,6 +13,7 @@ function HamburgerMenu(){
     )
 }
 
+// manually created close icon
 export function CloseIcon(){
     return(
         <div className="close-menu">
@@ -23,7 +25,6 @@ export function CloseIcon(){
 }
 
 export default function Navigation(props){
-
     const [navbarOpen, setNavbarOpen] = useState(false);
     const [navbarHide, setNavbarHide] = useState(false);
     
@@ -36,6 +37,7 @@ export default function Navigation(props){
         setNavbarOpen(false);
     }
 
+    // necessary to improve responsiveness of navigation open. if nav bar is open and the user scrolls the on the page, navigation bar will be temporarily disabled until user scrolls back up to the top of the page
     const changeValueOnScroll = () => {
         const scrollValue = document.documentElement.scrollTop;
         if(scrollValue > 25){
@@ -57,13 +59,14 @@ export default function Navigation(props){
                 <h1>Conscious</h1>
                 </div>
                 <button onClick={handleClick} type="button" className="hamburger-menu" alt="hamburger menu button. three horizontal lines stacked on eachother. once clicked to open the navigation options, hamburgermenu transitions to an X indicating to click again to leave navigation">
-                    {navbarOpen ? (
+                    {navbarOpen ? ( // conditional stying to either show a hamburger menu or a close menu depending on whether or not user has opened the navigation menu
                         <CloseIcon />
                         ) : (
                         <HamburgerMenu />
                         )
                     }
                 </button>
+                {/* conidtional styling for the nav-menu in order to the navigation bar to open only if the user clicks on the menu and activates the styling to show menu, otherwise hidden */}
                 <ul className={`nav-menu ${navbarOpen ? " show-menu" : ""} ${navbarHide ? " hide-menu" : ""}`}>
                     <li className="nav-item home"><Link to="/" className="nav-link" onClick={() => closeMenu()}>Home</Link></li>
                     <li className="nav-item find-brands"><Link to="/find-brands" className="nav-link" onClick={() => closeMenu()}>Find Brands</Link></li>
